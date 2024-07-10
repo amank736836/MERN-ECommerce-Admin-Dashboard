6fc165fc7eba6bf66110598f45af20054570fd93
Aman Kumar <amankarguwal0@gmail.com>
2024-07-10 12:55:45 +0530

    feat: Add Transaction page with AdminSidebar component

---
+++ src/pages/Transaction.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Transaction = () => {
+  return (
+    <div className="adminContainer">
+      <AdminSidebar />
+      <main>Transaction</main>
+    </div>
+  );
+};
+
+export default Transaction;
