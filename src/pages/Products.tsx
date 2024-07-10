b68bc49020639525d3c51b8b2a166e884376c707
Aman Kumar <amankarguwal0@gmail.com>
2024-07-10 12:55:35 +0530

    feat: Add Products page with AdminSidebar component

---
+++ src/pages/Products.tsx
@@ -0,0 +1,12 @@
+import AdminSidebar from "../components/AdminSidebar";
+
+const Products = () => {
+  return (
+    <div className="adminContainer">
+      <AdminSidebar />
+      <main>Products</main>
+    </div>
+  );
+};
+
+export default Products;
