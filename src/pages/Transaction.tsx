d3b3374bfea517bf130063b76fe67addbef7be2b
Aman Kumar <amankarguwal0@gmail.com>
2024-07-12 17:22:44 +0530

    refactor: Add lazy loading for images in Transaction page

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,10 +1,47 @@
+import { useState } from "react";
+
 import AdminSidebar from "../components/AdminSidebar/AdminSidebar";
+import TransactionTable, {
+  TransactionDataType,
+} from "../components/Tables/TransactionTable";
+import { Link } from "react-router-dom";
+
+const arr: TransactionDataType[] = [
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="red">Processing</span>,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 999,
+    discount: 450,
+    quantity: 6,
+    status: <span className="green">Shipped</span>,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 6999,
+    discount: 400,
+    quantity: 5,
+    status: <span className="purple">Delivered</span>,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+];
 
 const Transaction = () => {
+  const [transactions] = useState<TransactionDataType[]>(arr);
+
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main>Transaction</main>
+      <main>
+        <TransactionTable data={transactions} />
+      </main>
     </div>
   );
 };
