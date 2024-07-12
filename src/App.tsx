241a39204e57daf0b402ebddaa7aac859460f5f2
Aman Kumar <amankarguwal0@gmail.com>
2024-07-12 17:21:26 +0530

    refactor: Update import paths and component names for Dashboard and Product pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loader from "./components/Loader";
 const Dashboard = lazy(() => import("./pages/Dashboard"));
-const Products = lazy(() => import("./pages/Products"));
+const Product = lazy(() => import("./pages/Product"));
 const Transaction = lazy(() => import("./pages/Transaction"));
-const Customers = lazy(() => import("./pages/Customers"));
+const Customer = lazy(() => import("./pages/Customer"));
 
 const App = () => {
   return (
@@ -12,9 +12,9 @@ const App = () => {
       <Suspense fallback={<Loader/>}>
         <Routes>
           <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/product" element={<Product />} />
           <Route path="/admin/transaction" element={<Transaction />} />
-          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/customer" element={<Customer />} />
         </Routes>
       </Suspense>
     </Router>
