3ecc60f59f149f650a2eb963adef97f85b87142e
Aman Kumar <amankarguwal0@gmail.com>
2024-07-14 10:30:37 +0530

    refactor: Update DoughnutChart component to use destructured props

--- src/components/Charts/DoughnutChart.tsx
+++ src/components/Charts/DoughnutChart.tsx
@@ -2,48 +2,47 @@ import { ChartData, ChartOptions } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
 interface DoughnutChartProps {
-    data: number[];
-    labels: string[];
-    backgroundColor: string[];
-    cutout?: number | string;
-    legends?: boolean;
-    offset?: number[];
-  }
-  
-  export const DoughnutChart = ({
-    data,
+  data: number[];
+  labels: string[];
+  backgroundColor: string[];
+  cutout?: number | string;
+  legends?: boolean;
+  offset?: number[];
+}
+
+export const DoughnutChart = ({
+  data,
+  labels,
+  backgroundColor,
+  cutout,
+  legends = true,
+  offset,
+}: DoughnutChartProps) => {
+  const DoughnutChartData: ChartData<"doughnut", number[], string> = {
     labels,
-    backgroundColor,
-    cutout,
-    legends = true,
-    offset,
-  }: DoughnutChartProps) => {
-    const DoughnutData: ChartData<"doughnut", number[], string> = {
-      labels,
-      datasets: [
-        {
-          data,
-          backgroundColor,
-          borderWidth: 0,
-          offset,
-        },
-      ],
-    };
-  
-    const doughnutOptions: ChartOptions<"doughnut"> = {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: legends,
-          position: "bottom",
-          labels: {
-            padding: 40,
-          },
+    datasets: [
+      {
+        data,
+        backgroundColor,
+        borderWidth: 0,
+        offset,
+      },
+    ],
+  };
+
+  const DoughnutChartOptions: ChartOptions<"doughnut"> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: legends,
+        position: "bottom",
+        labels: {
+          padding: 40,
         },
       },
-      cutout,
-    };
-  
-    return <Doughnut data={DoughnutData} options={doughnutOptions} />;
+    },
+    cutout,
   };
-  
+
+  return <Doughnut data={DoughnutChartData} options={DoughnutChartOptions} />;
+};
