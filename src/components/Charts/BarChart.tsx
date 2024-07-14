5cae8bcccabf6200a62327f035c5432c2b23ed93
Aman Kumar <amankarguwal0@gmail.com>
2024-07-14 10:30:06 +0530

    refactor: Update BarChart component to use flex bar thickness and improve options

--- src/components/Charts/BarChart.tsx
+++ src/components/Charts/BarChart.tsx
@@ -1,40 +1,30 @@
 import { ChartData, ChartOptions } from "chart.js";
 import { Bar } from "react-chartjs-2";
 
-interface BarChartProps{
-    horizontal?: boolean;
-    data_1:number[];
-    data_2:number[];
-    title_1:string;
-    title_2:string;
-    bgColor_1:string;
-    bgColor_2:string;
-    labels?:string[];
+interface BarChartProps {
+  horizontal?: boolean;
+  data_1: number[];
+  data_2: number[];
+  title_1: string;
+  title_2: string;
+  bgColor_1: string;
+  bgColor_2: string;
+  labels?: string[];
 }
 
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    ];
+const months = ["January", "February", "March", "April", "May", "June", "July"];
 
-export const BarChart = (
-    {
-        horizontal = false,
-        data_1=[],
-        data_2=[],
-        title_1,
-        title_2,
-        bgColor_1,
-        bgColor_2,
-        labels = months,
-    }:BarChartProps
-) => {
-  const options:ChartOptions<"bar"> = {
+export const BarChart = ({
+  horizontal = false,
+  data_1 = [],
+  data_2 = [],
+  title_1,
+  title_2,
+  bgColor_1,
+  bgColor_2,
+  labels = months,
+}: BarChartProps) => {
+  const BarChartOptions: ChartOptions<"bar"> = {
     responsive: true,
     indexAxis: horizontal ? "y" : "x",
     plugins: {
@@ -46,44 +36,49 @@ export const BarChart = (
         text: "Chart.js Bar Chart",
       },
     },
-    scales:{
-        y:{
-            beginAtZero:true,
-            grid:{
-                display:false,
-            }
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
         },
-        x:{
-            beginAtZero:true,
-            grid:{
-                display:false,
-            }
-        }
-    }
+      },
+      x: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+      },
+    },
   };
 
-
-  const data:ChartData<"bar",number[],string> = {
+  const BarChartData: ChartData<"bar", number[], string> = {
     labels,
     datasets: [
       {
         label: title_1,
         data: data_1,
         backgroundColor: bgColor_1,
-        barThickness:"flex",
-        barPercentage:1,
-        categoryPercentage:0.4
-    },
-    {
+        barThickness: "flex",
+        barPercentage: 1,
+        categoryPercentage: 0.4,
+      },
+      {
         label: title_2,
         data: data_2,
         backgroundColor: bgColor_2,
-        barThickness:"flex",
-        barPercentage:1,
-        categoryPercentage:0.4
+        barThickness: "flex",
+        barPercentage: 1,
+        categoryPercentage: 0.4,
       },
     ],
   };
 
-  return <Bar data={data} options={options} />;
+  return (
+    <Bar
+      data={BarChartData}
+      options={BarChartOptions}
+      width={horizontal ? "200%" : ""}
+    />
+  );
 };
